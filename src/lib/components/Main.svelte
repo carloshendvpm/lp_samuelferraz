d3e7f6b18eee309ffc88295804f91cc1b997caf6
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-18 13:26:29 -0300

    changing from margin to gap

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -1,8 +1,8 @@
 <main class="flex items-center justify-center mt-6 flex-col px-3.5">
 	<p class="font-bold text-zinc-300 text-base mb-5 capitalize">Links úteis</p>
-	<ul>
-		<li class="text-center cursor-pointer border-2 font-medium text-zinc-300 text-sm p-3.5 px-16 mb-5 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Para entrar em contato, tirar dúvidas ou agendar um encontro comigo</a></li>
-		<li class="text-center cursor-pointer border-2 font-medium text-zinc-300 text-sm p-3.5 px-20 mb-5 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Playlist para Trabalhar</a></li>
-		<li class="text-center cursor-pointer border-2 font-medium text-zinc-300 text-sm p-3.5 px-16 mb-5 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Faça parte do ecossistema Samuel Ferraz</a></li>
+	<ul class="flex flex-col gap-5 ">
+		<li class="text-center cursor-pointer border-2 font-medium text-zinc-300 text-sm p-3.5 px-16 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Para entrar em contato, tirar dúvidas ou agendar um encontro comigo</a></li>
+		<li class="text-center cursor-pointer border-2 font-medium text-zinc-300 text-sm p-3.5 px-20 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Playlist para Trabalhar</a></li>
+		<li class="text-center cursor-pointer border-2 font-medium text-zinc-300 text-sm p-3.5 px-16 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Faça parte do ecossistema Samuel Ferraz</a></li>
 	</ul>
 </main>
