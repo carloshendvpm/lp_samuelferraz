e23ac1f8830317354e6b33ea6957357d820dfa43
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-18 08:42:29 -0300

    create links component to use inside header

---
+++ src/lib/components/Links.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { LinkedinBrand, InstagramBrand, WhatsappBrand } from 'svelte-awesome-icons'
+</script>
+<div class="icons-links flex mt-6 w-32 flex items-center justify-between mb-2">
+  <LinkedinBrand class="text-white hover:text-zinc-600 transition duration-150 ease-out hover:ease-in cursor-pointer" size=32/>
+  <InstagramBrand class="text-white hover:text-zinc-600 transition duration-150 ease-out hover:ease-in cursor-pointer" size=32/>
+  <WhatsappBrand class="text-white hover:text-zinc-600 transition duration-150 ease-out hover:ease-in cursor-pointer" size=32/>
+</div>
