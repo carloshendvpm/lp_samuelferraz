8ae57bb62fc67ef218b6fe9c3cebd947b16ba120
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-19 12:36:21 -0300

    refactoring styles

--- src/lib/components/Links.svelte
+++ src/lib/components/Links.svelte
@@ -1,7 +1,7 @@
 <script>
   import { LinkedinBrand, InstagramBrand, WhatsappBrand } from 'svelte-awesome-icons'
 </script>
-<div class="icons-links flex mt-6 w-32 flex items-center justify-between mb-2">
+<div class="icons-links flex mt-6 w-32 flex items-center gap-4 mb-2">
   <LinkedinBrand class="text-white hover:text-zinc-600 transition duration-150 ease-out hover:ease-in cursor-pointer" size=32/>
   <InstagramBrand class="text-white hover:text-zinc-600 transition duration-150 ease-out hover:ease-in cursor-pointer" size=32/>
   <WhatsappBrand class="text-white hover:text-zinc-600 transition duration-150 ease-out hover:ease-in cursor-pointer" size=32/>
--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -1,8 +1,8 @@
-<main class="flex items-center justify-center mt-6 flex-col px-3.5">
-	<p class="font-bold text-zinc-300 text-base mb-5 capitalize">Links úteis</p>
+<main class="flex items-center justify-center gap-6 mt-6 flex-col px-3.5">
+	<p class="font-bold text-zinc-300 text-base capitalize">Links úteis</p>
 	<ul class="flex flex-col gap-5 ">
-		<li class="text-center cursor-pointer border-2 font-medium text-zinc-300 text-sm p-3.5 px-16 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Para entrar em contato, tirar dúvidas ou agendar um encontro comigo</a></li>
-		<li class="text-center cursor-pointer border-2 font-medium text-zinc-300 text-sm p-3.5 px-20 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Playlist para Trabalhar</a></li>
-		<li class="text-center cursor-pointer border-2 font-medium text-zinc-300 text-sm p-3.5 px-16 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Faça parte do ecossistema Samuel Ferraz</a></li>
+		<li class="text-center cursor-pointer border-2 font-bold text-zinc-300 text-sm p-3.5 px-16 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Para entrar em contato, tirar dúvidas ou agendar um encontro comigo</a></li>
+		<li class="text-center cursor-pointer border-2 font-bold text-zinc-300 text-sm p-3.5 px-20 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Playlist para Trabalhar</a></li>
+		<li class="text-center cursor-pointer border-2 font-bold text-zinc-300 text-sm p-3.5 px-16 hover:bg-zinc-300 transition duration-150 ease-out hover:ease-in hover:text-zinc-800 hover:scale-110"><a href="">Faça parte do ecossistema Samuel Ferraz</a></li>
 	</ul>
 </main>
